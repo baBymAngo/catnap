1a84646974ebcb0dd01d03255e8d4635f1c4eb1e
Liam Scott <lcscott123@gmail.com>
2014-08-17 00:34:38 -0400

    Header Styles

--- site/snippets/header.php
+++ site/snippets/header.php
@@ -1,7 +1,7 @@
 <header>		
 	<div class="margin-container">		
 	    <a class="logo" href="<?= url('home') ?>"><img src="<?= url('assets/images/logo.svg') ?>" alt="<?= $site->title() ?>"></a>
-		<nav class="main-nav">
+		<nav class="main-menu">
 	    	<?php foreach($pages->visible() as $p): ?>
 	    	<a<?php echo ($p->isOpen()) ? ' class="active"' : '' ?> href="<?php echo $p->url() ?>"><?php echo html($p->title()) ?></a></li>
 	    	<?php endforeach ?>
