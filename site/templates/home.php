05d04413341e71c58a98735c8d4d01bb6a058516
Liam Scott <lcscott123@gmail.com>
2014-08-17 00:05:43 -0400

    Styles Update

--- site/templates/home.php
+++ site/templates/home.php
@@ -1,33 +1,30 @@
 <?php snippet('html-header') ?>
 
-<div class="wrapper">
+<main class="wrapper">
 	<div class="hero">
 		<?php snippet('header') ?>
 	</div>
 
-	<section>
-        <h1 class="section-title">Services</h1>
+	<section class="services">
+        <h1>Services</h1>
         <ul class="services-list">
-            <?php $services = $pages->find('services')->children()  ?>
+            <?php $services = $page->children()  ?>
             <?php foreach( $services as $service ): ?>
-                <li class="list-item">
-                    <a href="#">
-                        <i style="color: black; position: relative; left: 50%; -webkit-transform: translateX(-50%); margin-bottom: 25px;" class="fa fa-3x fa-file-image-o large-text-center"></i>
-                        <h3><?= $service->title() ?></h3>
-                    </a>
+                <li class="service">
+                    <h2><?= $service->title() ?></h2>
                     <p><?= $service->blurb() ?></p>
                 </li>
             <?php endforeach ?>
         </ul>
     </section>
 
-    <section class="social-footer">
+    <section class="instagram-section">
         <div class="instagram-wrapper">
             <div id="instagram"></div>
-             <div class="overlay"></div>
+            <div class="overlay"></div>
         </div>  
     </section>
-</div>
+</main>
 
 <script src="assets/js/instafeed.min.js"></script>
 <script src="assets/js/home.js"></script>
