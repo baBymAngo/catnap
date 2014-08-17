b276a47aafa35d06c0a07a85677182939dc2a570
Liam Scott <lcscott123@gmail.com>
2014-08-16 23:30:59 -0400

    Set-up file structure and snippets

--- site/snippets/header.php
+++ site/snippets/header.php
@@ -1,19 +1,10 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  
-  <title><?php echo html($site->title()) ?> - <?php echo html($page->title()) ?></title>
-  <meta charset="utf-8" />
-  <meta name="description" content="<?php echo html($site->description()) ?>" />
-  <meta name="keywords" content="<?php echo html($site->keywords()) ?>" />
-  <meta name="robots" content="index, follow" />
-
-  <?php echo css('assets/styles/styles.css') ?>
-
-</head>
-
-<body>
-
-  <header>
-    <h1><a href="<?php echo url() ?>"><img src="<?php echo url('assets/images/logo.png') ?>" width="115" height="41" alt="<?php echo html($site->title()) ?>" /></a></h1>
-  </header>
+<header>		
+	<div class="margin-container">		
+	    <a class="logo" href="<?= url('home') ?>"><img src="<?= url('assets/images/logo.svg') ?>" alt="<?= $site->title() ?>"></a>
+		<nav class="main-nav">
+	    	<?php foreach($pages->visible() as $p): ?>
+	    	<a<?php echo ($p->isOpen()) ? ' class="active"' : '' ?> href="<?php echo $p->url() ?>"><?php echo html($p->title()) ?></a></li>
+	    	<?php endforeach ?>
+    	</nav>
+  	</div>
+</header>
