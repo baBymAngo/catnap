0c6b792c2b74b58abe7f19a494cc333cd225c6da
Liam Scott <lcscott123@gmail.com>
2014-08-18 14:23:01 -0400

    Added Instagram Feed

--- assets/js/home.js
+++ assets/js/home.js
@@ -1,4 +1,5 @@
 (function(){
+	// Typed Script
 	var arr = $('.adjectives').data('adjectives').split(", ");
 	arr.forEach(function(element, index) {
 		arr[index] = element + ".";
@@ -18,4 +19,15 @@
 	   if( $this.height() > maxHeight ){ maxHeight = $this.height(); }
 	});
 	$services.height(maxHeight);
+
+	// Instagram Feed
+	var userFeed = new Instafeed({
+		target: 'instagram',
+        get: 'user',
+        userId: 224081856,
+        accessToken: '224081856.467ede5.2c3e471bd40b4a1ba781f0b877bba767',
+        template: '<a href="{{link}}" target="_blank" class="shot"><img src="{{image}}" /></a>',
+        limit: 12
+    });
+    userFeed.run();
 })();
