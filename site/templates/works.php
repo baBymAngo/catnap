3006e31a19641562df244d478ff38cb281e75ed5
Liam Scott <lcscott123@gmail.com>
2014-08-20 12:36:49 -0400

    Started Works Template

--- site/templates/works.php
+++ site/templates/works.php
@@ -1,66 +1,36 @@
+<!-- Template: Works -->
 <?php snippet('html-header') ?>
 
 <main class="projects-wrapper">
-	<div class="hero">
+	<div class="hero no-image">
         <div class="hero-bg">
     		<?php snippet('header') ?>
             <div class="hero-wrapper margin-container">
-                <h1 class="hero-header">Just some stuff we put together :)</h1>
-                <p class="hero-copy">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nostrum minus ex atque neque aut officiis corrupti tenetur cupiditate, repellat earum.</p>
+                <h1 class="hero-header"><?= $page->hero_header() ?></h1>
+                <p class="hero-copy"><?= $page->hero_subheader() ?></p>
             </div>
         </div>
 	</div>
 
-	<div id="project-scroller">
+	<div class-"projects-list">
 		<?php $projects = $page->children() ?>
-		<?php $index = 0 ?>
 		<?php foreach( $projects as $project ) : ?>
-			<?php if ($index%2==0) : ?>
-
-				<!-- EVEN PROJECT -->
-				<section class="project-section">
-					<article class="side side-left">
-						<h2><?= $project->title() ?></h2>
-						<p><?= $project->blurb() ?></p>
-						<div class="link-wrapper">
-							<a href="<?php $page->link() ?>">View Project</a>
-						</div>
+			<section class="project-block">
+				<div class="margin-container">
+					<article class="project-content">
+						<h1><?= $project->title() ?></h1>
+						<span class="responsibilities">
+							Responsibilities: <?= $project->responsibilities() ?>
+						</span>
+						<p><?= $project->description() ?></p>
+						<a href="<?php $page->link() ?>">View Site</a>
 					</article>
-
-					<figure class="side side-right">
-						<div class="ipad"><img src="http://placekitten.com/800/500" alt="<?= $project->title() ?>"></div>
-					</figure>
-				</section>
-				<?php $index++ ?>
-
-			<?php else : ?>
-
-				<!-- ODD PROJECT -->
-				<section class="project-section">
-					<figure class="side side-left">
-						<div class="ipad"><img src="http://placekitten.com/800/500" alt="<?= $project->title() ?>"></div>
-					</figure>
-
-					<article class="side side-right">
-						<h2><?= $project->title() ?></h2>
-						<p><?= $project->blurb() ?></p>
-						<div class="link-wrapper">
-							<a href="<?php $page->link() ?>">View Project</a>
-						</div>
-					</article>
-				</section>
-				<?php $index++ ?>
-
-		<?php endif ?>
+				</div>
+				<img src="http://placehold.it/650x400" alt="Project Thumbnail" class="project-thumb">
+			</section>
 		<?php endforeach ?>
 	</div>
 </main>
-<script src="../assets/js/vendors/classie.js"></script>
-<script src="../assets/js/vendors/scroller.min.js"></script>
-<script>
-(function(){
-	new cbpScroller( document.getElementById('project-scroller') );
-})();	
-</script>
+	
 <?php snippet('footer') ?>
 <?php snippet('html-footer') ?>
