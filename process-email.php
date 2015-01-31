64535a6b534a35d596aae21b3e19392c7c3b2f85
Liam Scott <lcscott123@gmail.com>
2015-01-30 21:54:00 -0500

    Added HTML to contact emails

--- process-email.php
+++ process-email.php
@@ -11,6 +11,8 @@ if ( $_SERVER["REQUEST_METHOD"] == "POST" ) {
 	$address = filter_var(trim($_POST['address']), FILTER_SANITIZE_EMAIL);
 	$message = trim( $_POST['message'] );
 
+	$confirmationMessage = "Hey $firstName,We\'ve received your message and will respond within 48 hours, although we usually reply much sooner than that. If for some reason you don't hear from us, send another email to bonjour@catnap.co and we'll get back to you as soon as possible.\n\nThanks,\n\nKyle & Liam\nTeam Catnap\ncatnap.co";
+
 	// Check that all data was sent
 	if ( empty($firstName) OR empty($message) OR !filter_var($address, FILTER_VALIDATE_EMAIL) ) {
 		echo "user error";
@@ -19,7 +21,7 @@ if ( $_SERVER["REQUEST_METHOD"] == "POST" ) {
 
 	// Send emails
 	$email = mail( 'bonjour@catnap.co', "A message from $firstName $lastName", $message, "From: $address");
-	$confirmation = mail( $address, 'We got your note', "Hey $firstName,\n\nWe\'ve received your message and will respond within 48 hours, although we usually reply much sooner than that. If for some reason you don't hear from us, send another email to bonjour@catnap.co and we'll get back to you as soon as possible.\n\nThanks,\n\nKyle & Liam\nTeam Catnap\ncatnap.co" , 'From: bonjour@catnap.co' );
+	$confirmation = mail( $address, 'We got your note',  $confirmationMessage, 'From: bonjour@catnap.co\r\nContent-Type: text/html' );
 
 	if ( $email && $confirmation ) {
 		echo "success";
